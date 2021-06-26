ec519a1abcd4d0aa91c19a535e9b25fd17c2d093
CarlosReinoso <bless@carlosreinoso.co.uk>
2021-06-26 18:22:21 +0100

    added run build file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path")
 require("dotenv").config();
 // const knex = require("knex")(require("./knexfile")["development"])
 
